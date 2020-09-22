457007bb23f82d9e4b89f624a9acd5bbad239641
songwy <2811797251@qq.com>
2020-09-22 10:15:21 +0800

    添加git测试文件

--- src/main/java/com/songwy/utils/file/GitTest.java
+++ src/main/java/com/songwy/utils/file/GitTest.java
@@ -1,8 +1,12 @@
 package com.songwy.utils.file;
-
+import java.util.Date;
 public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("本地生成git账号进行测试。。。");
+        System.out.println(System.currentTimeMillis());
+        System.out.println(new Date());
+
+
     }
 }
